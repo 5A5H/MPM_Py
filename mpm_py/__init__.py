5110d06ecec121ef98f1bc02e4be8a4c56294306
Sascha Maassen <sascha.maassen@uni-due.de>
2020-02-03 10:09:07 +0100

    Restructure to make a package

---
+++ mpm_py/__init__.py
@@ -0,0 +1,4 @@
+print('Welcome to MPM_Py!')
+from mpm_py.Grid import Grid
+from mpm_py.Body import Body
+#__all__ = ["Grid", "Body.Body"] #this was reported by docu but does not work as expected (at least ^^)
